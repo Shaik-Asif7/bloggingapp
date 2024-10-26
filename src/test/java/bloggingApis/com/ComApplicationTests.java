d1bb0e6418aa79763ea3b54ac88132ea68e64eff
ShaikAsif7 <asif.shaik@troinfotech.in>
2024-10-26 15:38:11 +0530

    initial commit

---
+++ src/test/java/bloggingApis/com/ComApplicationTests.java
@@ -0,0 +1,13 @@
+package bloggingApis.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

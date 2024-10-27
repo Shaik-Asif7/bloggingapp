6e5cd81288f3196d57084d599933cac9c8792b62
ShaikAsif77 <asif.shaik@troinfotech.in>
2024-10-27 19:22:45 +0530

    testing

--- src/main/java/bloggingApis/com/controller/UserCont.java
+++ src/main/java/bloggingApis/com/controller/UserCont.java
@@ -2,6 +2,9 @@ package bloggingApis.com.controller;
 
 public class UserCont {
     public static void main(String[] args) {
+        public static void add(){
+            System.out.println("User added successfully");
+        }
         
     }
 }
